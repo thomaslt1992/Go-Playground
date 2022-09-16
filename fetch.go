ab2613673c95930a39a119d78015f63520c586c0
thomaslt1992 <thomas.tasioulis92@gmail.com>
2022-09-16 14:50:56 +0300

    checking adding prefix

---
+++ fetch.go
@@ -0,0 +1,37 @@
+// Fetch prints the content found at a URL.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+
+		var pref string
+		if !strings.HasPrefix(url, "http://") {
+			pref = "http://"
+			url = pref + url
+			fmt.Println("No prefix found, added the http:// prefix ")
+			fmt.Println("The new url is ", url)
+		}
+		fmt.Println("Sending request ...")
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
